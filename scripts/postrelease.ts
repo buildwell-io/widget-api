d5908f76519d800bded8c98a95654d43e15eecab
Almost-Infinity <its.alfinity@gmail.com>
2024-02-12 13:03:10 +0200

    refactor: update `postrelease` script

--- scripts/postrelease.ts
+++ scripts/postrelease.ts
@@ -1,5 +1,6 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 
+import { execSync } from 'child_process';
 import { PackageJson } from 'nx/src/utils/package-json';
 
 function getCurrentTimestamp(): string {
@@ -9,14 +10,24 @@ function getCurrentTimestamp(): string {
         .join('.');
 }
 
-function syncProjectConstants(): void {
-    const { version } = JSON.parse(readFileSync('package.json').toString()) as PackageJson;
-
+function syncProjectConstants(version: string): void {
     const versionConstant = `export const APP_VERSION = '${version}';`;
     const dateConstant = `export const APP_VERSION_AT = '${getCurrentTimestamp()}'; // UTC`;
     writeFileSync('src/app/constants/version.ts', `${versionConstant}\n${dateConstant}\n`, { encoding: `utf-8` });
 }
 
+function updateGitIndexAndPush(version: string): void {
+    execSync('git add ./package.json', { stdio: `inherit` });
+    execSync('git add ./src/app/constants/version.ts', { stdio: `inherit` });
+    execSync(`git commit -a -n -m 'chore(release): ${version}'`);
+    execSync(`git tag -a v${version} -m "Release ${version}"`);
+    execSync('git push --set-upstream origin', { stdio: 'inherit', encoding: 'utf-8' });
+    execSync('git push --set-upstream origin --tags', { stdio: 'inherit', encoding: 'utf-8' });
+}
+
 (function main(): void {
-    syncProjectConstants();
+    const { version } = JSON.parse(readFileSync('package.json').toString()) as PackageJson;
+
+    syncProjectConstants(version);
+    updateGitIndexAndPush(version);
 })();
